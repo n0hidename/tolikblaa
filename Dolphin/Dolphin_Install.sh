9b12bc91bc562996d1127410facbaa27cfa05444
n0hidename <124586806+n0hidename@users.noreply.github.com>
2023-02-06 13:31:28 +0000

    Update Dolphin_Install.sh

--- Dolphin/Dolphin_Install.sh
+++ Dolphin/Dolphin_Install.sh
@@ -2,8 +2,8 @@
 
 sudo apt update && sudo apt install gnome-terminal -y && sudo apt install openvpn -y
 
-mkdir -p /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers
-wget -O /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers/gologin.tar https://dl.gologin.com/gologin.tar
+mkdir -p /home/$(whoami)/tolikblaa/Dolphin/Browsers
+wget -O /home/$(whoami)/tolikblaa/Dolphin/Browsers/gologin.tar https://dl.gologin.com/gologin.tar
 tar –xvf gologin.tar 
 
 OUTPUT=$(whoami)
@@ -11,23 +11,23 @@ OUTPUT=$(whoami)
 var="[Desktop Entry]
 Type=Application
 Terminal=true
-Exec=sudo xfce4-terminal -e 'sudo bash /home/${OUTPUT}/hgytkfdfgtyuuuuu/OpenVpn/OpenVpn.sh'
+Exec=sudo xfce4-terminal -e 'sudo bash /home/${OUTPUT}/tolikblaa/OpenVpn/OpenVpn.sh'
 Name=OpenVPN
-Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/OpenVpn/OpenVPN.png"
+Icon=/home/${OUTPUT}/tolikblaa/OpenVpn/OpenVPN.png"
 echo "$var" > "/home/$(whoami)/Desktop/OpenVPN.Desktop"
 
 var="[Desktop Entry]
 Type=Application
 Terminal=false
-Exec=sudo /home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/Browsers/GoLogin-3.2.6 --no-sandbox
+Exec=sudo /home/${OUTPUT}/tolikblaa/Dolphin/Browsers/GoLogin-3.2.6 --no-sandbox
 Name=Gologin
-Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/DolphinLogo.png"
+Icon=/home/${OUTPUT}/tolikblaa/Dolphin/DolphinLogo.png"
 echo "$var" > "/home/$(whoami)/Desktop/Gologin.Desktop"
 
 sudo chmod ugo+rwx /home/$(whoami)/Desktop/Dolphin.Desktop
 sudo chmod ugo+rwx /home/$(whoami)/Desktop/OpenVPN.Desktop
-sudo chmod ugo+rwx /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers/GoLogin-3.2.6
-sudo chmod ugo+rwx /home/$(whoami)/hgytkfdfgtyuuuuu/OpenVpn/OpenVpn.sh
+sudo chmod ugo+rwx /home/$(whoami)/tolikblaa/Dolphin/Browsers/GoLogin-3.2.6
+sudo chmod ugo+rwx /home/$(whoami)/tolikblaa/OpenVpn/OpenVpn.sh
 
 exit 0
 END
